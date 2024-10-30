bc560851ba58999e82a5de9b01e03eec974972c5
Loner <163582383+Nosferatzod@users.noreply.github.com>
2024-10-30 15:55:05 -0300

    Portifolio

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,15 @@ function closeModal(id) {
 function toggleMenu() {
     document.body.classList.toggle('nav-active');
 }
+
+const toggleButton = document.getElementById('toggleButton');
+const navbarNav = document.getElementById('navbarNav');
+const closeButton = document.getElementById('closeButton');
+
+toggleButton.addEventListener('click', () => {
+    navbarNav.classList.toggle('active');
+});
+
+closeButton.addEventListener('click', () => {
+    navbarNav.classList.remove('active');
+});
